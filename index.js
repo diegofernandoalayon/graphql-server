371f4849079411357ee9228e4064972df3e27782
Diego Fernando Alayon <dfar.41@gmail.com>
2021-12-19 11:57:50 -0500

    definiciones en graphql y resolvers

---
+++ index.js
@@ -0,0 +1,46 @@
+import { ApolloServer, gql } from 'apollo-server'
+
+const persons = [
+  {
+    name: 'dfar',
+    phone: '3333 3333',
+    street: 'calle 3 4-23',
+    city: 'Mia',
+    id: '123412341234'
+  },
+  {
+    name: 'aoeuaoe',
+    phone: '3122332 3333',
+    street: 'calle 4 4-23',
+    city: 'tuya',
+    id: '63456345634'
+  },
+  {
+    name: 'pedro',
+    phone: '1233 3344',
+    street: 'calle 5 4-23',
+    city: 'suya',
+    id: '12348765845'
+  },
+]
+const typeDefs = gql`
+  type Person {
+    name: String!
+    phone: String
+    street: String!
+    city: String!
+    id: ID!
+  }
+
+  type Query {
+    personCount: Int!
+    allPersons: [Person]!
+  }
+`
+
+const resolvers = {
+  Query: {
+    personCount: () => persons.length,
+    allPersons: () => persons
+  }
+}

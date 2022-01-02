c70a470879aa42f4b27c5da575ed9d21ea04073e
Diego Fernando Alayon <dfar.41@gmail.com>
2022-01-02 11:38:08 -0500

    realizando coneccion con base de datos,

---
+++ db.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose'
+
+const { MONGODB_URI } = process.env
+
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+})
+.then(()=>{
+  console.log('connected to MongoDB')
+}).catch(error => {
+  console.error('error connection to MongoDB' ,error.message)
+})
